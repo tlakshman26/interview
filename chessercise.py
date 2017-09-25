7bbdf15e2deac53ab23cc721761414f1c1d0ec76
lakshman <lakshminarayanat@vedams.com>
2017-09-25 18:05:54 +0530

    Added to get most distant tile

--- chessercise.py
+++ chessercise.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 import sys, getopt
+import random
+
 def main(argv):
     piece = ''
     position = ''
@@ -9,13 +11,51 @@ def main(argv):
     gameboard = {}
     chessCardinals = [(1,0),(0,1),(-1,0),(0,-1)]
     chessDiagonals = [(1,1),(-1,1),(1,-1),(-1,-1)]
+    target = False
+    len_r_keys = 8
+    opposing_pieces = []
 
     for i in range(8):
         for j, char in enumerate(range(97, 105)):
-            gameboard[(i,j)] = "{0}{1}".format(chr(char), i+1)
+            gameboard[(i,j)] = '  ' #"{0}{1}".format(chr(char), i+1)
+
+    def random_position():
+        keys = gameboard.keys()
+        r_keys = []
+        while True:
+            if len(r_keys) > len_r_keys:
+                break
+            r_key = random.choice(keys)
+            if r_key not in r_keys:
+                gameboard[r_key] = ' b'
+                r_keys.append(r_key)
+        print "r_key", r_keys
+        return r_keys
+
+    def printBoard():
+        print(" |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |")
+        for i in range(8):
+            print("-"*42)
+            print chr(i+97)+'|',
+            for j in range(8):
+                item = gameboard.get((i,j),"  ")
+                print str(item)+' |',
+            print
+        print("-"*42)
+
+    def get_most_distant_tile(position):
+        target_position = None
+        target_list = {}
+        if opposing_pieces:
+            for i in opposing_pieces:
+                value = abs(i[0]-position[0])+abs(i[1]-position[1])
+                target_list[i] = value
+        if target_list:
+            target_position = max(target_list, key=target_list.get)
+        return target_position
     
     try:
-        opts, args = getopt.getopt(argv, "hp:n:",["piece=","position="])
+        opts, args = getopt.getopt(argv, "hp:n:",["target", "piece=","position="])
     except getopt.GetoptError as e:
         print 'chessercise.py -piece <KNIGHT> -position <d2>'
         sys.exit(2)
@@ -35,15 +75,21 @@ def main(argv):
                 position_index = (ord(arg[0])-97, int(arg[1])-1)[::-1]
             except:
                 pass
-            if gameboard.get(position_index) == arg:
+            if arg: #gameboard.get(position_index) == arg:
                 position = arg
             else:
                 print 'Make sure position value is correct.'
                 sys.exit(3)
+        elif opt in ("--target"):
+            target = True
 
     if not (piece and  position):
         print 'chessercise.py -piece <KNIGHT> -position <d2>'
         sys.exit()
+    if target:
+        opposing_pieces=random_position()
+        printBoard()
+        most_distant_tile = get_most_distant_tile(position_index)
     
     x, y = position_index
     
